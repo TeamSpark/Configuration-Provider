0763cc9c64597eb151a08fc87ba143f95ea5f52e
Anton Boyko <boyko.ant@live.com>
2015-07-02 22:34:35 +0300

    introduce IConfigurationReader and ConfigurationReader

---
+++ TeamSpark.ConfigurationProvider.Infrastructure/ConfigurationReader.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TeamSpark.ConfigurationProvider.Infrastructure
+{
+	public sealed class ConfigurationReader : IConfigurationReader
+	{
+		private List<IConfigurationReader> _configurationReaders = new List<IConfigurationReader>();
+
+		#region Singleton
+
+		private static Lazy<ConfigurationReader> _instance = new Lazy<ConfigurationReader>(() => { return new ConfigurationReader(); });
+
+		public static ConfigurationReader Instance
+		{
+			get
+			{
+				return _instance.Value;
+			}
+		}
+
+		private ConfigurationReader()
+		{
+		}
+
+		#endregion
+
+		
+		#region IConfigurationReader
+
+		public string GetConfigurationValue(string key, string defaultValue = null)
+		{
+			foreach (var reader in _configurationReaders)
+			{
+				var configurationValue = reader.GetConfigurationValue(key, defaultValue);
+				if (configurationValue != defaultValue)
+				{
+					return configurationValue;
+				}
+			}
+
+			return defaultValue;
+		}
+
+		#endregion
+	}
+}
---
+++ TeamSpark.ConfigurationProvider.Infrastructure/IConfigurationReader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TeamSpark.ConfigurationProvider.Infrastructure
+{
+	public interface IConfigurationReader
+	{
+		string GetConfigurationValue(string key, string defaultValue = null);
+	}
+}
