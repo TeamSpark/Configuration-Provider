ae0e63f8d47271d622aadf4ae2d1efd18894de10
Anton Boyko <boyko.ant@live.com>
2015-07-02 22:50:32 +0300

    introduce InMemoryConfigurationReader

---
+++ TeamSpark.ConfigurationProvider.General/InMemoryConfigurationReader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using TeamSpark.ConfigurationProvider.Infrastructure;
+
+namespace TeamSpark.ConfigurationProvider.General
+{
+	public sealed class InMemoryConfigurationReader : IConfigurationReader
+	{
+		private Dictionary<string, string> _configurationVault = new Dictionary<string,string>();
+
+		public void AddOrUpdateConfiguration(string key, string value)
+		{
+			if (_configurationVault.ContainsKey(key))
+			{
+				_configurationVault[key] = value;
+			}
+			else
+			{
+				_configurationVault.Add(key, value);
+			}
+		}
+
+		public string GetConfigurationValue(string key, string defaultValue = null)
+		{
+			return _configurationVault.ContainsKey(key) ?
+				_configurationVault[key] :
+				defaultValue;
+		}
+	}
+}
